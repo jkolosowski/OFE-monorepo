a88296524f09f3b00aa246b57dcb3d8bdcb9cd58
Jakub Kołosowski <kuba.kolosowski@icloud.com>
2025-05-15 09:48:35 +0200

    feat: add conventional commits and bump storybook version

--- packages/core-components/.storybook/main.ts
+++ packages/core-components/.storybook/main.ts
@@ -1,13 +1,19 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/web-components-vite';
 
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  stories: ['../src/components/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  addons: [
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    "@chromatic-com/storybook"
+  ],
+
   framework: {
-    name: '@storybook/web-components-vite',
+    name: getAbsolutePath("@storybook/web-components-vite"),
     options: {},
   },
 
@@ -15,6 +21,10 @@ const config: StorybookConfig = {
     mergeConfig(config, {
       plugins: [nxViteTsPaths()],
     }),
+
+  docs: {
+    autodocs: true
+  }
 };
 
 export default config;
@@ -22,3 +32,7 @@ export default config;
 // To customize your Vite configuration you can use the viteFinal field.
 // Check https://storybook.js.org/docs/react/builders/vite#configuration
 // and https://nx.dev/recipes/storybook/custom-builder-configs
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
